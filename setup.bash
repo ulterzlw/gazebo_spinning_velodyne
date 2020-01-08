ef36b55c64d84be52b1e90d61b63e70c4ed04d05
AlterEtch <linwei.zheng.alter@gmail.com>
2020-01-09 01:27:27 +0800

    feat: setup without install

--- setup.bash
+++ setup.bash
@@ -1,10 +1,9 @@
 BASE=$(cd "`dirname "${BASH_SOURCE[0]}"`";pwd)
 cd $BASE
-mkdir build
-cd build
+mkdir $BASE/build
+cd $BASE/build
 cmake ..
 make
-cp -r $BASE/models ~/.gazebo
-echo $(cd $BASE/build;pwd)
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(cd $BASE/build;pwd)
 cd -
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(cd $BASE/build;pwd)
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(cd $BASE/models;pwd)
