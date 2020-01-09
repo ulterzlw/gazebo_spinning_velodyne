dd62817f182c995c6303505f805c22d0c358ca5e
AlterEtch <linwei.zheng.alter@gmail.com>
2020-01-09 17:02:58 +0800

    chore: only set env in setup.bash

--- setup.bash
+++ setup.bash
@@ -1,9 +1,3 @@
 BASE=$(cd "`dirname "${BASH_SOURCE[0]}"`";pwd)
-cd $BASE
-mkdir $BASE/build
-cd $BASE/build
-cmake ..
-make
-cd -
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(cd $BASE/build;pwd)
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(cd $BASE/models;pwd)
